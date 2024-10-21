8a453e5dfbe5423f145a6466bb9b2ecc0efc365d
tushar-454 <imtushar454@gmail.com>
2024-10-22 00:10:04 +0600

    add a program to check a number is prime or not

---
+++ check-one-prime-number/main.f95
@@ -0,0 +1,15 @@
+    PRINT*, 'ENTER THE VALUE OF N'
+    READ* , N
+    K=2
+7   IF(N.EQ.(N/K) *K) GOTO 5
+    PRINT*, N,K, '-- after goto 5 '
+    K=K+1
+    PRINT*, N,K, '-- after goto K+ '
+    IF(K.LE.N/2) GOTO 7
+    PRINT*, N,K, '-- after goto 7 '
+    PRINT*, N, 'IS A PRIME NO. '
+    STOP
+    PRINT*, N,K, '-- before print 5 '
+5   PRINT*, N,'IS NOT A PRIME NO.'
+    PRINT*, K, 'IS A DIVISOR OF' ,N
+    END
