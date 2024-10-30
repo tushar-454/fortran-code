268f2f5f699e6c8d4649c0b2b25c9993303b33fb
tushar-454 <imtushar454@gmail.com>
2024-10-30 12:42:53 +0600

    solve the first quesiton series sum

--- math-lab-ii-fortran/quesiton-1/main.f90
+++ math-lab-ii-fortran/quesiton-1/main.f90
@@ -1,3 +1,15 @@
-program Hello
-    print*, "Hello fortarn"
-end program Hello
+! write a Fortran program to find sum of the series:
+! 1^2-2^2+3^2-4^2+....+(-1)^nN^2.
+
+program series_sum
+    SUM = 0
+    I = -1
+    K = 1
+    PRINT*, "Enter the last number of series:"
+    READ*, N
+    DO K = 1, N
+        SUM = SUM + (-1*I)*K**2
+        I = I * -1
+    END DO
+    PRINT*, "Sum of the series:", SUM
+end program series_sum
