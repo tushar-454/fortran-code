a92ce7f2fa943bbf4507cd18fc003d86673bdff3
tushar-454 <imtushar454@gmail.com>
2024-10-31 16:11:57 +0600

    solve the 16th(iii) quesiton GET_40_BELOW_STUDENT_NUMBER and refactor i,ii

--- math-lab-ii-fortran/quesiton-16-iii/main.f90
+++ math-lab-ii-fortran/quesiton-16-iii/main.f90
@@ -1,7 +1,7 @@
 ! A class of 100 students takes an examination on which scores range from 0 to 100.Write a Fortran program which finds
-! ii)The number of students who scored above 80
+! iii)The number of students who score below 40
 
-PROGRAM GET_80_PLUS_STUDENT_NUMBER
+PROGRAM GET_40_BELOW_STUDENT_NUMBER
 
     IMPLICIT NONE
     INTEGER :: STUDENTS, I, SCORE, STUDENT
@@ -14,15 +14,15 @@ PROGRAM GET_80_PLUS_STUDENT_NUMBER
     DO I = 1, STUDENTS
         PRINT*, "Enter the score for STUDENT", I, ":"
         READ*, SCORE
-         IF(SCORE.LT.0.0 .OR. SCORE.GT.100.0) THEN
+        IF(SCORE.LT.0.0 .OR. SCORE.GT.100.0) THEN
             PRINT*, "Score must be greater then 0 and Less then 100. Try Again please."
             STOP
         END IF
-        IF(SCORE.GT.80) THEN
+        IF(SCORE.LT.40) THEN
             STUDENT = STUDENT + 1
         END IF
     END DO
 
-    PRINT*, "The number of students who scored above 80 is:", STUDENT
+    PRINT*, "The number of students who scored below 40 is:", STUDENT
 
-END PROGRAM GET_80_PLUS_STUDENT_NUMBER
+END PROGRAM GET_40_BELOW_STUDENT_NUMBER
