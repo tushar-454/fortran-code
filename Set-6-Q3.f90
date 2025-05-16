1abfb07309a41a85617f2da17cafba6926370748
Tushar Imran <imtushar454@gmail.com>
2025-05-16 18:21:43 +0600

    Refactor Gauss-Jordan elimination program to improve variable naming and streamline input handling

--- Set-6-Q3.f90
+++ Set-6-Q3.f90
@@ -1,45 +1,31 @@
 ! Gauss-Jordan elimination to solve a 3x3 system of linear equations
-! System:
-! x1 + 3x2 + 8x3 =  4
-! x1 + 4x2 + 3x3 = -2
-! x1 + 3x2 + 4x3 =  1
-
 program gauss_jordan
     implicit none
     integer :: i, j
-    real :: a(3,4), factor, temp
-    real :: x(3)
+    real :: aug(3,4), factor, temp
+    real :: roots(3)
 
-    ! Initialize the augmented matrix [A|b]
-    a = reshape([ &
-        1.0, 3.0, 8.0,  4.0, &
-        1.0, 4.0, 3.0, -2.0, &
-        1.0, 3.0, 4.0,  1.0  &
-    ], [3,4])
+    print*, "Enter augmented matrix value:(row wise)"
+    read*, ((aug(i, j), j = 1, 4), i = 1, 3)
 
-    ! Gauss-Jordan elimination for 3x3 system
     do i = 1, 3
-        ! Make the diagonal element 1
-        temp = a(i,i)
-        a(i,:) = a(i,:) / temp
+        temp = aug(i,i)
+        aug(i,:) = aug(i,:) / temp
 
-        ! Make other elements in column i zero
         do j = 1, 3
             if (j .NE. i) then
-                factor = a(j,i)
-                a(j,:) = a(j,:) - factor * a(i,:)
+                factor = aug(j,i)
+                aug(j,:) = aug(j,:) - factor * aug(i,:)
             end if
         end do
     end do
 
-    ! Extract solution
     do i = 1, 3
-        x(i) = a(i, 4)
+        roots(i) = aug(i, 4)
     end do
 
-    ! Print the solution
     print *, 'Solution using Gauss-Jordan elimination:'
-    print *, 'x1 = ', x(1)
-    print *, 'x2 = ', x(2)
-    print *, 'x3 = ', x(3)
+    print *, 'x = ', roots(1)
+    print *, 'y = ', roots(2)
+    print *, 'z = ', roots(3)
 end program gauss_jordan
